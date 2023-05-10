8d8e81098d0e22e3a5fabc3eb210749174d7a89d
Mikalai Shcharbakou <doosterhere@gmail.com>
2023-05-10 22:44:37 +0300

    the next changes have been made:
    1. package.json (backend) has been changed in the script section: a new
       script "start" was added and the former "start" became "dev";
    2. package.json (frontend) has been changed in the script section: a new
       script "build-gp" was added;
    3. index.html has been changed in the <head> section: <base> section was
       added with href connected with htmlWebpackPlugin.options.baseUrl;
    4. A new path config for a remote backend was added (pathConfig.prod.js);
    5. webpack.config.js was configured for a development build and for a
       production build ('build' and 'build-gp' scripts in package.json
       accordingly);

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,52 +2,50 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-    entry: './src/app.js',
-    mode: "development",
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-        }),
-        new CopyPlugin({
-            patterns: [
-                {from: "templates", to: "templates"},
-                {from: "styles", to: "styles"},
-                {from: "static/fonts", to: "fonts"},
-                {from: "static/images", to: "images"},
-                {from: "config", to: "config"},
-                {from: "node_modules/bootstrap/dist/css", to: "bootstrap/css"},
-                {from: "node_modules/bootstrap/dist/js", to: "bootstrap/js"},
-                {from: "node_modules/jquery/dist", to: "jquery"},
-                {from: "node_modules/@popperjs/core/dist/umd", to: "@popperjs"},
-                {from: "node_modules/bootstrap-datepicker/dist/js", to: "datepicker/js"},
-                {from: "node_modules/bootstrap-datepicker/dist/locales", to: "datepicker/locales"},
-                {from: "node_modules/bootstrap-datepicker/dist/css", to: "datepicker/css"},
-            ],
-        }),
-    ],
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.js$/,
-    //             exclude: /(node_modules)/,
-    //             use: {
-    //                 loader: 'babel-loader',
-    //                 options: {
-    //                     presets: ['@babel/preset-env']
-    //                 }
-    //             }
-    //         }
-    //     ]
-    // },
-    devServer: {
-        static: ".dist",
-        compress: true,
-        port: 9000,
-    },
+module.exports = (env, argv) => {
+    let baseUrl = "/";
+    let outputPath = path.resolve(__dirname, 'dist');
+    let patterns = [
+        { from: "templates", to: "templates" },
+        { from: "styles", to: "styles" },
+        { from: "static/fonts", to: "fonts" },
+        { from: "static/images", to: "images" },
+        { from: "config", globOptions: { ignore: ["**/*prod.js"] }, to: "config" },
+        { from: "node_modules/bootstrap/dist/css", to: "bootstrap/css" },
+        { from: "node_modules/bootstrap/dist/js", to: "bootstrap/js" },
+        { from: "node_modules/jquery/dist", to: "jquery" },
+        { from: "node_modules/@popperjs/core/dist/umd", to: "@popperjs" },
+        { from: "node_modules/bootstrap-datepicker/dist/js", to: "datepicker/js" },
+        { from: "node_modules/bootstrap-datepicker/dist/locales", to: "datepicker/locales" },
+        { from: "node_modules/bootstrap-datepicker/dist/css", to: "datepicker/css" },
+    ];
+    let pattern = {};
+
+    if (argv.mode === 'production') {
+        outputPath = path.resolve(__dirname, 'docs');
+        baseUrl = "/lumincoin-finance/";
+        pattern = { from: "config/pathConfig.prod.js", to: "config/pathConfig.js", force: true };
+    }
+
+    return {
+        entry: './src/app.js',
+        mode: "none",
+        output: {
+            filename: 'main.js',
+            path: outputPath,
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./index.html",
+                baseUrl: baseUrl
+            }),
+            new CopyPlugin({
+                patterns: !!Object.keys(pattern).length ? (() => {
+                    patterns.push(pattern);
+                    return patterns;
+                })() : patterns,
+            }),
+        ]
+    };
 };
